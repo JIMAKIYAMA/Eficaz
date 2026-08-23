1fd881a45f094c5310a543d66f10a2253f20bab4
Jim Akiyama <jimakiyama314@gmail.com>
2026-08-22 23:21:44 -0300

    inicial

---
+++ servidor.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template_string, redirect, request
+import views 
+
+app = Flask(__name__)
+
+app.static_folder = 'static'
+
+@app.route('/')
+def index():
+    return render_template_string(views.index())
+
+@app.route('/submit', methods=['POST'])
+def submitar():
+    titulo = request.form.get('titulo_nome')
+    detalhe = request.form.get('detalhe_nome')
+    views.submit(titulo, detalhe)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+from flask import render_template_string
+
+def load_data(html):
+    caminho = os.path.join(os.path.dirname(__file__), "static", "data", html)
+    print(f"caminho ========== {caminho}")
+    with open(caminho, 'r', encoding='utf-8') as file:
+        dados_json = json.load(file)
+
+    return dados_json
+
+def load_template(html):
+    lista = html.split('/')
+    caminho = os.path.join(os.path.dirname(__file__), "static", "templates", *lista)
+    with open(caminho, 'r') as file:
+        dados = file.read()
+    return dados
+
